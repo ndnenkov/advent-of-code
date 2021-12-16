9970b34aff82afd24835d6073f8d08be1435d133
Nikola Nenkov <ndnenkov@gmail.com>
2021-12-16 22:21:29 +0200

    Day 14 solutions

---
+++ 2021/15/a.ts
@@ -0,0 +1,64 @@
+import _ from 'lodash'
+
+const input = `
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+`
+
+const cave = input
+  .trim()
+  .split('\n')
+  .map(line => line.split('').map(risk => +risk))
+
+function neighbors(cave: number[][], x: number, y: number): [number, number][] {
+  return [
+    [0, -1],
+    [0, 1],
+    [1, 0],
+    [-1, 0]
+  ]
+    .map(([dx, dy]) => [x + dx, y + dy] as [number, number])
+    .filter(([nx, ny]) => cave[ny]?.[nx] !== undefined)
+}
+
+function dijkstra(
+  cave: number[][],
+  startX: number,
+  startY: number,
+  endX: number,
+  endY: number
+): number {
+  const queue = [[startX, startY]]
+  const visited = new Set<string>()
+  visited.add(`${startX},${startY}`)
+
+  const distances = cave.map(row => row.map(() => Infinity))
+  distances[startY][startX] = 0
+
+  while (queue.length) {
+    const minDistance = _.min(_.flatMap(queue.map(([x, y]) => distances[y][x])))!
+    const nextIndexInQueue = queue.findIndex(([x, y]) => distances[y][x] === minDistance)
+    const [x, y] = queue.splice(nextIndexInQueue, 1)[0]
+
+    neighbors(cave, x, y).forEach(([nx, ny]) => {
+      distances[ny][nx] = _.min([distances[ny][nx], distances[y][x] + cave[ny][nx]])!
+
+      if (!visited.has(`${nx},${ny}`)) {
+        queue.push([nx, ny])
+        visited.add(`${nx},${ny}`)
+      }
+    })
+  }
+
+  return distances[endY][endX]
+}
+
+console.log(dijkstra(cave, 0, 0, cave[0].length - 1, cave.length - 1))
